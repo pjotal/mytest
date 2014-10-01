0b6461c6d7a115ef4256b001a357849f9819509b
Pedro Carneiro <pedro.carneiro@lightspeedretail.com>
2014-10-01 16:21:52 -0400

    this is my commit msg

--- src/FirstModule.py
+++ src/FirstModule.py
@@ -1,18 +1,22 @@
-'''
-Created on May 28, 2014
 
-@author: sandbox
-'''
-def add(a,b):
-    return a+b
+
+def add(a, b):
+    return a + b
+
 
 def addFixedValue(a):
     y = 5
     return y + a
 
-def sample(variable):
+
+def sample(variable):  # @UnusedVariable
+    """
+    blabla
+    @param variable:
+    """
     print "this is my thrid update"
 
-print add(1,2)
+
+print add(1, 2)
 print addFixedValue(1)
 sample("pedro")
