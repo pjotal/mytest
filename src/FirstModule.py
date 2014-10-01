8f72bf73d99e40734b5b39d1fc99a6eda34b8ed2
Pedro Carneiro <pedro.carneiro@lightspeedretail.com>
2014-10-01 16:38:39 -0400

    fixed warnings

--- src/FirstModule.py
+++ src/FirstModule.py
@@ -1,18 +1,15 @@
-'''
-Created on May 28, 2014
+def add(a, b):
+    return a + b
 
-@author: sandbox
-'''
-def add(a,b):
-    return a+b
 
 def addFixedValue(a):
     y = 5
     return y + a
 
-def sample(variable):
+
+def sample(variable):  # @UnusedVariable
     print "this is my thrid update"
 
-print add(1,2)
+print add(1, 2)
 print addFixedValue(1)
 sample("pedro")
